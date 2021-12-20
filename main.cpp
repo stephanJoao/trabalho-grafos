2768c4b99d1d4e94064a2c0491ab00f8ecfd20c1
antonio <antonio.medeiros@estudante.ufjf.br>
2021-12-20 20:09:52 -0300

    chamando a ordenacao topologica na main

--- main.cpp
+++ main.cpp
@@ -61,39 +61,39 @@ Graph* readGraph(ifstream& input_file, bool directed, bool weighted_edge, bool w
 
 int main(int argc, char const *argv[]) 
 {
-    /* // Verifies if all parameters have been provided */
-    /* if (argc != 6) */ 
-    /* { */
-    /*     cout << "ERROR: Expecting: ./<program_name> <input_file> <output_file> <directed> <weighted_edge> <weighted_vertex> " << endl; */
-    /*     return 1; */
-    /* } */
+    // Verifies if all parameters have been provided
+    if (argc != 6) 
+    {
+        cout << "ERROR: Expecting: ./<program_name> <input_file> <output_file> <directed> <weighted_edge> <weighted_vertex> " << endl;
+        return 1;
+    }
 
-    /* string program_name(argv[0]); */
-    /* string input_file_name(argv[1]); */
-    /* std::cout << "Input file: " << input_file_name << std::endl; */
-    /* string output_file_name(argv[2]); */
-    /* std::cout << "Output file: " << output_file_name << std::endl; */
-    /* bool directed = atoi(argv[3]); */
-    /* std::cout << "Directed: " << directed << std::endl; */
-    /* bool weighted_edge = atoi(argv[4]); */
-    /* std::cout << "Weighted edge: " << weighted_edge << std::endl; */
-    /* bool weighted_vertex = atoi(argv[5]); */
-    /* std::cout << "Weighted vertex: " << weighted_vertex << std::endl; */
+    string program_name(argv[0]);
+    string input_file_name(argv[1]);
+    std::cout << "Input file: " << input_file_name << std::endl;
+    string output_file_name(argv[2]);
+    std::cout << "Output file: " << output_file_name << std::endl;
+    bool directed = atoi(argv[3]);
+    std::cout << "Directed: " << directed << std::endl;
+    bool weighted_edge = atoi(argv[4]);
+    std::cout << "Weighted edge: " << weighted_edge << std::endl;
+    bool weighted_vertex = atoi(argv[5]);
+    std::cout << "Weighted vertex: " << weighted_vertex << std::endl;
 
 
-    /* // Read of input_file */
-    /* ifstream input_file; */
-    /* input_file.open(input_file_name, ios::in); */
+    // Read of input_file
+    ifstream input_file;
+    input_file.open(input_file_name, ios::in);
 
-    /* Graph *g; */
-    /* if(input_file.is_open()){ */
+    Graph *g;
+    if(input_file.is_open()){
 
-    /*     g = readGraph(input_file, atoi(argv[3]), atoi(argv[4]), atoi(argv[5])); */
+        g = readGraph(input_file, atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
 
-    /* } else { */
-    /*     cout << "Unable to open " << input_file_name << std::endl; */
-    /*     exit(1); */
-    /* } */
+    } else {
+        cout << "Unable to open " << input_file_name << std::endl;
+        exit(1);
+    }
 
     // // leitura do arquivo de saida
     // ofstream output_file;
@@ -106,18 +106,36 @@ int main(int argc, char const *argv[])
     // g->insertEdge(1, 4);
     // g->insertEdge(2, 3);
     
-    Graph *g = new Graph(0, true, false, false);
-    g->insertEdge(1, 2);
-    g->insertEdge(2, 3);
-    g->insertEdge(3, 4);
-    g->insertEdge(3, 5);
-    g->insertEdge(5, 4);
+    // Graph *g = new Graph(8, false, true, false);
+    // g->insertEdge(1, 2, 5);
+    // g->insertEdge(1, 3, 8);
+    // g->insertEdge(1, 6, 7);
+    // g->insertEdge(1, 8, 6);
+    // g->insertEdge(2, 3, 4);
+    // g->insertEdge(2, 4, 8);
+    // g->insertEdge(2, 5, 7);
+    // g->insertEdge(2, 8, 9);
+    // g->insertEdge(3, 5, 4);
+    // g->insertEdge(3, 6, 5);
+    // g->insertEdge(4, 5, 9);
+    // g->insertEdge(4, 7, 4);
+    // g->insertEdge(5, 6, 3);
+    // g->insertEdge(5, 7, 10);
+    // g->insertEdge(6, 7, 6);
 
     // g->printAdjList();
-    // g->BFS(1);
-    g->saveToDot("graph1.dot");
+    // g->saveToDot("graph1.dot");
     // g->Dijkstra(0, 1);
+
+    std::set<std::pair<int, int>> *mst_edges = g->MST_Kruskal();
+    g->saveToDot("mst_kruskal.dot", mst_edges);
+
+    std::set<std::pair<int, int>> *back_edges = new std::set<std::pair<int, int>>;
+    std::set<std::pair<int, int>> *tree_edges = g->BFS(1, back_edges);
+    g->saveToDot("bfs_tree.dot", tree_edges, back_edges);
+
     g->topologicalSorting();
+
     delete g;
 
     input_file.close();
